6aad2dc40b0c0d767bb728e0dbcf19b97fb2dba7
kirkirey <55382480+kirkirey@users.noreply.github.com>
2019-09-16 11:37:04 +0300

    Update main.py

--- lab_1/main.py
+++ lab_1/main.py
@@ -6,7 +6,7 @@ Count frequencies dictionary by the given arbitrary text
 
 def calculate_frequences(text: str) -> dict:
     """
-    Calculates number of times each word appears in the text
+    Calculates number of times each word appears in the text хахахах
     """
     pass
 
