5fa2cb8b4b9dcece358ae9715df58053a8c247ae
VyuPechinkina <kirkirey@gmail.com>
2019-11-11 21:08:45 +0300

    Added code for all the functions given in the lab work 2

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,89 +1,60 @@
 def calculate_frequences(text: str) -> dict:
-    """
-    Calculates number of times each word appears in the text
-    """
+    if text != None:
+        text = str(text).lower().replace('\n', ' ')
+    else:
+        return {}
+    junk = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '%', '^', '&', '*', '`', '~', '&','.',
+            ',', ';', ':', '?','(', ')', '[', ']', '{', '}', '-', '=', '_', '+', ';', ':','|','/','"',"'"]
+    clean_text = ''
+    junk_found = False
+    for i in text:
+        for j in junk:
+            if i == j:
+                junk_found = True
+                break
+        if junk_found == False:
+            clean_text += i
+        junk_found = False
+    freq_source = clean_text.split(' ')
     frequencies = {}
-    new_text = ''
-    if text is None:
-        return frequencies
-    if not isinstance(text, str):
-        text = str(text)
-    for symbol in text:
-        if symbol.isalpha() or symbol == ' ':
-            new_text += symbol
-    new_text = new_text.lower()
-    words = new_text.split()
-    for key in words:
-        key = key.lower()
-        if key in frequencies:
-            value = frequencies[key]
-            frequencies[key] = value + 1
+    for i in range(len(freq_source)):
+        if freq_source[i] == '':
+            pass
+        elif freq_source[i] not in frequencies.keys():
+            frequencies[freq_source[i]] = 1
         else:
-            frequencies[key] = 1
+            frequencies[freq_source[i]] += 1
     return frequencies
-
 
 def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
-    """
-    Removes all stop words from the given frequencies dictionary
-    """
-    if frequencies is None:
-        frequencies = {}
+    if frequencies == None:
+        return {}
+    elif stop_words == None:
         return frequencies
-    for word in list(frequencies):
-        if not isinstance(word, str):
-            del frequencies[word]
-    if not isinstance(stop_words, tuple):
-        return frequencies
-    for word in stop_words:
-        if not isinstance(word, str):
-            continue
-        if frequencies.get(word) is not None:
-            del frequencies[word]
-    return frequencies
-
+    else:
+        frequencies_clean = dict(frequencies)
+        stop_words_correct = []
+        for i in stop_words:
+            stop_words_correct.append(str(i))
+        for i in frequencies_clean.keys():
+            if type(i) != str:
+                stop_words_correct.append(i)
+        for i in stop_words_correct:
+            if i in frequencies_clean.keys():
+                frequencies_clean.pop(i)
+        return frequencies_clean
 
 def get_top_n(frequencies: dict, top_n: int) -> tuple:
-    """
-    Takes first N popular words
-    :param
-    """
-    if not isinstance(top_n, int):
-        frequencies = ()
-        return frequencies
-    if top_n < 0:
-        top_n = 0
-    elif top_n > len(frequencies):
-        top_n = len(frequencies)
-    top_words = sorted(frequencies, key=lambda x: int(frequencies[x]), reverse=True)
-    best = tuple(top_words[:top_n])
-    return best
-
-
-def read_from_file(path_to_file: str, lines_limit: int) -> str:
-    """
-    Read text from file
-    """
-    file = open(path_to_file)
-    counter = 0
-    text = ''
-    if file is None:
-        return text
-    for line in file:
-        text += line
-        counter += 1
-        if counter == lines_limit:
-            break
-    file.close()
-    return text
-
-
-def write_to_file(path_to_file: str, content: tuple):
-    """
-    Creates new file
-    """
-    file = open(path_to_file, 'w')
-    for i in content:
-        file.write(i)
-        file.write('\n')
-    file.close()
+    freq_top_list = []
+    if top_n >= len(frequencies):
+        return tuple(frequencies.keys())
+    for i in range(top_n):
+        k = 0
+        for key, value in frequencies.items():
+            if value >= k:
+                top_word = key
+                k = value
+        else:
+            freq_top_list.append(top_word)
+            frequencies.pop(top_word)
+    return tuple(freq_top_list)
--- lab_2/main.py
+++ lab_2/main.py
@@ -4,15 +4,68 @@ Labour work #2. Levenshtein distance.
 
 
 def generate_edit_matrix(num_rows: int, num_cols: int) -> list:
-    pass
+    matrix = []
+    if type(num_rows) is not int or type(num_cols) is not int:
+        return matrix
+    else:
+        for i in range(num_rows):
+            this_row = []
+            for j in range(num_cols):
+                this_row.append(0)
+            matrix.append(this_row)
+        return matrix
+
+
+def initialize_edit_matrix2(edit_matrix: tuple, add_weight: int, remove_weight: int) -> list:
+    if type(edit_matrix) is list:
+        edit_matrix = tuple(edit_matrix)
+    if type(add_weight) is not int or type(remove_weight) is not int or edit_matrix is ():
+        return list(edit_matrix)
+    count = remove_weight
+    for i in edit_matrix:
+        i[0] = count
+        i.append(0)
+        count += remove_weight
+    first_row = tuple([[0]])
+    for i in range(len(edit_matrix[0]) - 1):
+        first_row[0].append(first_row[0][i] + add_weight)
+    matrix = first_row + edit_matrix
+    return list(matrix)
+
+
+def generate_edit_matrix2(num_rows: int, num_cols: int) -> list:
+    matrix = []
+    if type(num_rows) is not int or type(num_cols) is not int:
+        return matrix
+    else:
+        for i in range(num_rows + 1):
+            this_row = []
+            for j in range(num_cols + 1):
+                this_row.append(0)
+            matrix.append(this_row)
+        return matrix
 
 
 def initialize_edit_matrix(edit_matrix: tuple, add_weight: int, remove_weight: int) -> list:
-    pass
+    if type(edit_matrix) is list:
+        edit_matrix = tuple(edit_matrix)
+    if type(add_weight) is not int or type(remove_weight) is not int or edit_matrix is () or [] in edit_matrix:
+        return list(edit_matrix)
+    count = 0
+    for i in edit_matrix:  # заполнение первого столбца
+        i[0] = count
+        count += remove_weight
+    for i in range(len(edit_matrix[0]) - 1):  # заполнение первой строчки
+        edit_matrix[0][i + 1] = edit_matrix[0][i] + add_weight
+    return list(edit_matrix)
 
 
 def minimum_value(numbers: tuple) -> int:
-    pass
+    minimum = numbers[0]
+    for i in numbers:
+        if i < minimum:
+            minimum = i
+    return minimum
 
 
 def fill_edit_matrix(edit_matrix: tuple,
@@ -21,7 +74,19 @@ def fill_edit_matrix(edit_matrix: tuple,
                      substitute_weight: int,
                      original_word: str,
                      target_word: str) -> list:
-    pass
+    if type(add_weight) is not int or type(remove_weight) is not int or type(substitute_weight) is not int or type(
+            original_word) is not str or type(target_word) is not str:
+        return list(edit_matrix)
+    for i in range(len(edit_matrix) - 1):  # строка:   edit_matrix[i]  элемент:    edit_matrix[i][j]
+        for j in range(len(edit_matrix[i]) - 1):  # (len(*)-1) потому что я изменяю [i+1][j+1]
+            vertical = edit_matrix[i][j + 1] + remove_weight
+            horizontal = edit_matrix[i + 1][j] + add_weight
+            if original_word[i] == target_word[j]:
+                diagonal = edit_matrix[i][j]
+            else:
+                diagonal = edit_matrix[i][j] + substitute_weight
+            edit_matrix[i + 1][j + 1] = minimum_value(tuple([vertical, horizontal, diagonal]))
+    return list(edit_matrix)
 
 
 def find_distance(original_word: str,
@@ -29,4 +94,10 @@ def find_distance(original_word: str,
                   add_weight: int,
                   remove_weight: int,
                   substitute_weight: int) -> int:
-    pass
+    if type(add_weight) is not int or type(remove_weight) is not int or type(substitute_weight) is not int or type(
+            original_word) is not str or type(target_word) is not str:
+        return -1
+    matrix = generate_edit_matrix(len(original_word) + 1, len(target_word) + 1)
+    matrix = initialize_edit_matrix(tuple(matrix), add_weight, remove_weight)
+    matrix = fill_edit_matrix(tuple(matrix), add_weight, remove_weight, substitute_weight, original_word, target_word)
+    return matrix[len(original_word)][len(target_word)]
