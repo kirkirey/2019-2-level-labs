6e06d8aa56abb733835baea1e4b34d4bfb80f299
kirkirey <55382480+kirkirey@users.noreply.github.com>
2019-12-17 03:47:49 +0300

    laboratory work #4
    finished ver

--- lab_4/main.py
+++ lab_4/main.py
@@ -1,28 +1,83 @@
 import math
 
-
 REFERENCE_TEXTS = []
 
 
 def clean_tokenize_corpus(texts: list) -> list:
-    pass
+    corpus = []
+    for text in texts:
+        text_source = ''
+        corpus_source = []
+        if not isinstance(text, str):
+            corpus.append([])
+        else:
+            text = text.lower().replace('\n', ' ')
+        for letter in text:
+            if letter.isalpha() or letter is ' ':
+                text_source += letter
+        else:
+            corpus_source = text_source.split(' ')
+            while '' in corpus_source:
+                corpus_source.remove('')
+        corpus.append(corpus_source)
+    return corpus
 
 
 class TfIdfCalculator:
     def __init__(self, corpus):
-        pass
+        self.corpus = corpus
+        self.tf_values = []
+        self.idf_values = {}
+        self.tf_idf_values = []
 
     def calculate_tf(self):
-        pass
+        if self.corpus:
+            for text in self.corpus:
+                if not isinstance(text, list):
+                    continue  # скип
+                tf_value = {}
+                for word in text:
+                    if not isinstance(word, str):
+                        continue  # скип
+                    if word not in tf_value:
+                        tf_value[word] = text.count(word) / len(text)
+                self.tf_values.append(tf_value)
+        return self.tf_values
 
     def calculate_idf(self):
-        pass
+        if self.corpus:
+            for text in self.corpus:
+                if not isinstance(text, list):
+                    continue  # скип
+                for word in text:
+                    if not isinstance(word, str):
+                        continue  # скип
+                    word_freq_in_corpus = 0
+                    for text_2 in self.corpus:
+                        if not isinstance(text_2, list):
+                            continue  # скип
+                        if word in text_2:
+                            word_freq_in_corpus += 1
+                    self.idf_values[word] = math.log(len(self.corpus) / word_freq_in_corpus)
+        return self.idf_values
 
     def calculate(self):
-        pass
+        if self.tf_values and self.idf_values:
+            for tf_values in self.tf_values:
+                tf_idf_value = {}
+                for word, tf_value in tf_values.items():
+                    tf_idf_value[word] = tf_value * self.idf_values[word]
+                self.tf_idf_values.append(tf_idf_value)
+        return self.tf_idf_values
 
     def report_on(self, word, document_index):
-        pass
+        try:
+            tf_idf_value = self.tf_idf_values[document_index][word]
+        except TypeError:
+            return ()
+        whew = sorted(self.tf_idf_values[document_index], key=lambda a: int(self.tf_idf_values[document_index][a]),
+                      reverse=True)
+        return tf_idf_value, whew.index(word)
 
 
 if __name__ == '__main__':
@@ -33,8 +88,8 @@ if __name__ == '__main__':
     # scenario to check your work
     test_texts = clean_tokenize_corpus(REFERENCE_TEXTS)
     tf_idf = TfIdfCalculator(test_texts)
-    tf_idf.calculate_tf()
-    tf_idf.calculate_idf()
-    tf_idf.calculate()
+    print('tf_tf.calculate_idf() ', tf_idf.calculate_tf())
+    print('tf_idf.calculate_idf() ', tf_idf.calculate_idf())
+    print('tf_idf.calculate() ', tf_idf.calculate())
     print(tf_idf.report_on('good', 0))
     print(tf_idf.report_on('and', 1))
